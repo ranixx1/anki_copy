c70a4d8f79031d44eeee7ccd01e072f7be404297
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-02-17 19:29:26 -0300

    Initialize project with Maven, Spring Boot, and basic application structure

---
+++ src/main/java/com/example/anki_copy/AnkiCopyApplication.java
@@ -0,0 +1,13 @@
+package com.example.anki_copy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkiCopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnkiCopyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/anki_copy/AnkiCopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anki_copy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnkiCopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
