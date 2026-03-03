1ea5b23d7ca4e24736192c981635bcbf531a5885
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-03-02 23:28:17 -0300

    feat: adiciona método de procurar por nome no repository JPA

--- src/main/java/com/example/anki_copy/repository/DeckRepository.java
+++ src/main/java/com/example/anki_copy/repository/DeckRepository.java
@@ -1,9 +1,11 @@
 package com.example.anki_copy.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.anki_copy.model.Deck;
 
 public interface DeckRepository extends JpaRepository<Deck, Integer>{
-    
+    Optional<Deck> findByNome(String nome);
 }
