64f0b4d1ac7479fe1b281c6ccde4d43256098fa6
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-03-02 23:30:16 -0300

    feat: uma vez que restController, não precida da marcação @controller

--- src/main/java/com/example/anki_copy/controller/CardController.java
+++ src/main/java/com/example/anki_copy/controller/CardController.java
@@ -3,7 +3,6 @@ package com.example.anki_copy.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
