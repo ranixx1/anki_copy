5216fc26d0fef7dcd6a5ad3bfc0ac88108c88393
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-03-02 23:29:15 -0300

    feat: implementa criarDeck, listar decks e detalhar deck

--- src/main/java/com/example/anki_copy/service/DeckService.java
+++ src/main/java/com/example/anki_copy/service/DeckService.java
@@ -1,15 +1,34 @@
 package com.example.anki_copy.service;
 
-import com.example.anki_copy.repository.CardRepository;
+import java.util.List;
+
+import com.example.anki_copy.model.Card;
+import com.example.anki_copy.model.Deck;
 import com.example.anki_copy.repository.DeckRepository;
 
 public class DeckService {
-    private DeckRepository deckreRepository;
-    private CardRepository cardRepository;
-
-    public DeckService(DeckRepository deckRepository, CardRepository cardRepository) {
-        this.deckreRepository = deckRepository;
-        this.cardRepository = cardRepository;
+    private DeckRepository deckRepository;
+    public DeckService(DeckRepository deckRepository) {
+        this.deckRepository = deckRepository;
     }
 
+    public Deck criarDeck(String nome) {
+        Deck deck = new Deck();
+        deck.setNome(nome);
+        return deckRepository.save(deck);
+    }
+
+    public List<Deck> listarDecks() {
+        var decks = deckRepository.findAll();
+        if (decks.isEmpty()) {
+            throw new RuntimeException("Não há Decks");
+        }
+        return decks;
+    }
+
+    public List<Card> detalharDeck(String nome) {
+        Deck deck = deckRepository.findByNome(nome)
+                .orElseThrow(() -> new RuntimeException("Deck não encontrado."));
+        return deck.getCards();
+    }
 }
