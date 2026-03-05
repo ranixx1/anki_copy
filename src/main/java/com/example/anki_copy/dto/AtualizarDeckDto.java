ffd7a3061fc110dd8a76569e21a2dea4d467fb14
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-03-04 23:28:03 -0300

    feat: cria dto para funcionalidades do deck

---
+++ src/main/java/com/example/anki_copy/dto/AtualizarDeckDto.java
@@ -0,0 +1,10 @@
+package com.example.anki_copy.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class AtualizarDeckDto {
+    private String name;
+}
---
+++ src/main/java/com/example/anki_copy/dto/CriarDeckDto.java
@@ -0,0 +1,11 @@
+package com.example.anki_copy.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class CriarDeckDto {
+    private String nome;
+}
