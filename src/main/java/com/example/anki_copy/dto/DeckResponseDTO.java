08950a3202098f4d1c82b8c5123b0273d9382e37
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-03-05 00:04:12 -0300

    feat: cria dto para listagem de decks

---
+++ src/main/java/com/example/anki_copy/dto/DeckResponseDTO.java
@@ -0,0 +1,15 @@
+package com.example.anki_copy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class DeckResponseDTO {
+    private Integer id;
+    private String name;
+
+}
