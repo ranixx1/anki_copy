24a4fbb9178b686d565df7a3d28478cf4e6b0402
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-03-07 20:16:47 -0300

    fix

--- src/main/java/com/example/anki_copy/controller/DeckController.java
+++ src/main/java/com/example/anki_copy/controller/DeckController.java
@@ -1,9 +1,12 @@
 package com.example.anki_copy.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.anki_copy.dto.AtualizarDeckDto;
 import com.example.anki_copy.dto.CriarDeckDto;
+import com.example.anki_copy.dto.DeckResponseDTO;
+import com.example.anki_copy.model.Deck;
 import com.example.anki_copy.service.DeckService;
 
 @RestController
@@ -19,8 +24,8 @@ import com.example.anki_copy.service.DeckService;
 public class DeckController {
     private DeckService deckService;
 
-    @GetMapping()
-    public ResponseEntity<Void> criarCard(@RequestBody CriarDeckDto dto){
+    @PostMapping()
+    public ResponseEntity<Void> criarCard(@RequestBody CriarDeckDto dto) {
         deckService.criarDeck(dto.getNome());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
@@ -31,4 +36,10 @@ public class DeckController {
         return ResponseEntity.noContent().build();
 
     }
+
+    @GetMapping
+    public ResponseEntity<List<DeckResponseDTO>> listarDecks() {
+        return ResponseEntity.ok(deckService.listarDecks());
+    }
+
 }
--- src/main/java/com/example/anki_copy/repository/DeckRepository.java
+++ src/main/java/com/example/anki_copy/repository/DeckRepository.java
@@ -3,8 +3,10 @@ package com.example.anki_copy.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.anki_copy.model.Deck;
+@Repository
 
 public interface DeckRepository extends JpaRepository<Deck, Integer>{
     Optional<Deck> findByNome(String nome);
